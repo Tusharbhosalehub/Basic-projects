9707ec578ddecdc3a8e940936bc60beaa0c8007e
Tushar Bhosale <114877970+Tusharbhosalehub@users.noreply.github.com>
2023-12-31 22:05:40 +0530

    Update Tic_Tac_game.py

--- Tic_Tac_game.py
+++ Tic_Tac_game.py
@@ -1,15 +1,15 @@
-# Set up the game board as a list
+# Set up the game board as a list.
 board = ["-", "-", "-",
 		"-", "-", "-",
 		"-", "-", "-"]
 
-# Define a function to print the game board
+# Define a function to print the game board.
 def print_board():
 	print(board[0] + " | " + board[1] + " | " + board[2])
 	print(board[3] + " | " + board[4] + " | " + board[5])
 	print(board[6] + " | " + board[7] + " | " + board[8])
 
-# Define a function to handle a player's turn
+# Define a function to handle a player's turn.
 def take_turn(player):
 	print(player + "'s turn.")
 	position = input("Choose a position from 1-9: ")
@@ -21,7 +21,7 @@ def take_turn(player):
 	board[position] = player
 	print_board()
 
-# Define a function to check if the game is over
+# Define a function to check if the game is over.
 def check_game_over():
 	# Check for a win
 	if (board[0] == board[1] == board[2] != "-") or \
@@ -40,7 +40,7 @@ def check_game_over():
 	else:
 		return "play"
 
-# Define the main game loop
+# Define the main game loop.
 def play_game():
 	print_board()
 	current_player = "X"
@@ -55,8 +55,8 @@ def play_game():
 			print("It's a tie!")
 			game_over = True
 		else:
-			# Switch to the other player
+			# Switch to the other player.
 			current_player = "O" if current_player == "X" else "X"
 
-# Start the game
+# Start the game.
 play_game()
